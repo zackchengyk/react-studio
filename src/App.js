6cfb86c21b98057c6dfebbbb28620955d88318bc
Zack Cheng <zack_cheng@brown.edu>
2022-11-13 13:53:25 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import "./App.css";
-import { useState } from "react";
-import bakeryData from "./assets/bakery-data.json";
+import './App.css';
+import { useState } from 'react';
+import bakeryData from './assets/bakery-data.json';
+import BakeryItem from './components/BakeryItem.js';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+  item.image = (process.env.PUBLIC_URL + '/' + item.image).replace('//', '/');
 });
 /* ############################################################## */
 
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState({});
+
+  const price = Object.entries(cart).reduce(
+    (acc, [index, quantity]) => acc + bakeryData[index].price * quantity,
+    0
+  );
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>My Bakery</h1>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <div>
+        <h2>Menu</h2>
+        {bakeryData.map((item, index) => (
+          <BakeryItem item={item} index={index} setCart={setCart} />
+        ))}
+      </div>
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div className="cart">
+          {Object.entries(cart).map(([index, quantity]) => (
+            <p>
+              {quantity} x {bakeryData[index].name}
+            </p>
+          ))}
+          {Object.entries(cart).length === 0 && 'Your cart is empty.'}
+          {Object.entries(cart).length !== 0 && (
+            <p>{'Total = ' + formatter.format(price)}</p>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,29 @@
-// TODO: create a component that displays a single bakery item
+import { useState } from 'react';
+
+function BakeryItem({ item, index, setCart }) {
+  return (
+    <div
+      className="bakery-item"
+      style={{
+        cursor: 'pointer',
+        backgroundColor: '#eee',
+        borderRadius: '1rem',
+        display: 'flex',
+        gap: '1rem',
+        alignItems: 'center',
+        overflow: 'hidden',
+      }}
+      onClick={() =>
+        setCart((prev) => {
+          const quantity = prev[index] ?? 0;
+          return { ...prev, [index]: quantity + 1 };
+        })
+      }
+    >
+      <img src={item.image} style={{ width: '5rem' }} />
+      {item.name}
+    </div>
+  );
+}
+
+export default BakeryItem;
